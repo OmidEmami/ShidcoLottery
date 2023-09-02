e35e1ae7aa376f139af8958f5c068226cfbd8660
Omid Emami <omidem1781@gmail.com>
2023-09-02 12:52:18 +0330

    finalized

--- src/component/Lottery.js
+++ src/component/Lottery.js
@@ -3,8 +3,13 @@ import styles from "./Lottery.module.css";
 import logomotion from "./looplogo.gif";
 import Modal from 'react-modal';
 import useWindowSize from 'react-use/lib/useWindowSize';
-import Confetti from 'react-confetti'
+import Confetti from 'react-confetti';
+import logo from './logo.png';
+import CountUp from 'react-countup';
+import ReactAudioPlayer from 'react-audio-player';
+import effect from "./effect.mp3"
 function Lottery() {
+ 
     const { width, height } = useWindowSize();
     const customStyles = {
         content: {
@@ -24,33 +29,51 @@ function Lottery() {
     const [randomNumbers, setRandomNumbers] = useState([]);
     const [showGif, setShowGif] = useState(false);
     const [isCele, setIsCele] = useState(false);
-    const [countWon, setCountWon] = useState()
+    const [countWon, setCountWon] = useState();
+    const [countWinner , setCountWinner] = useState();
+    const [timeWinner , setTimeWinner] = useState();
+    const [seconderPopup,setSeconderPopup]=useState(false);
+    const [tempWinner, setTempWinner] = useState(0);
+    const [playMusic, setPlayMusic] = useState(false)
     const startLottery = async(e)=>{
+     
         e.preventDefault();
-        const count = 2;
+       setPlayMusic(true)
     const newRandomNumbers = [];
 
-    for (let i = 0; i < count; i++) {
+    for (let i = 0; i < countWinner; i++) {
+      if(timeWinner !== "0"){
         setCountWon(i + 1)
+        setSeconderPopup(false)
         setIsCele(false)
         setInitialPopup(false)
-        setShowGif(true)
-        await new Promise((resolve) => setTimeout(resolve, 13000));
+       
+        await new Promise((resolve) => setTimeout(resolve,  timeWinner*1000));
         const randomNumber = Math.floor(Math.random() * (parseInt(secondNumber) - parseInt(firstNumber) + 1)) + parseInt(firstNumber);
-      newRandomNumbers.push(`shidco - ${randomNumber}`);
-      setRandomNumbers([...newRandomNumbers]);
+      newRandomNumbers.push(randomNumber);
       
-      setShowGif(false);
+      setTempWinner(randomNumber)
+      setSeconderPopup(true);
+      await new Promise((resolve) => setTimeout(resolve, 4.5*1000));
       setPopupValue(randomNumber)
       setIsCele(true)
       setInitialPopup(true)
-      await new Promise((resolve) => setTimeout(resolve, 10000));
+      setRandomNumbers([...newRandomNumbers]);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }else{
+      const randomNumber = Math.floor(Math.random() * (parseInt(secondNumber) - parseInt(firstNumber) + 1)) + parseInt(firstNumber);
+      newRandomNumbers.push(randomNumber);
+      setRandomNumbers([...newRandomNumbers]);
+      setIsCele(true)
+      await new Promise((resolve) => setTimeout(resolve, 400));
     }
+    }
+    await new Promise((resolve) => setTimeout(resolve, 10000));
     setIsCele(false)
     setInitialPopup(false)
-    setShowGif(false)
+    
     setShowEnd(true)
-    //setRandomNumbers(newRandomNumbers);
+    setPlayMusic(false)
     
     }
     const resetForm = async(e)=>{
@@ -59,7 +82,52 @@ function Lottery() {
         setSecondNumber('');
     }
   return (
+    <>
+    <div className={styles.fullContainer}>
+      <div className={styles.header}>
+      <img src={logo} />
+      <h3>کارخانه تجهیزات آموزشی شیدکو</h3>
+      <h5>سیستم قرعه کشی هوشمند مبتنی بر هوش مصنوعی</h5>
+      </div>
     <div className={styles.mainDivContainer}>
+    <div>
+      <h2>برندگان</h2>
+        <ul>
+        {randomNumbers.length > 0 ? randomNumbers.map((number, index) => (
+          <li style={{color:"blue"}} key={index}>{number}</li>
+        )):<p>منتظر بمانید...</p>}
+      </ul>
+    </div>
+      <div className={styles.formContainer}>
+        <form onSubmit={(e)=>startLottery(e)} className={styles.lotteryFrom}>
+        <label>شماره اولین کارت قرعه کشی</label>
+        <input required type='number' placeholder='شروع' value={firstNumber} onChange={(e)=>setFirstNumber(e.target.value)} />
+        <label>شماره آخرین کارت قرعه کشی</label>
+        <input required type='number' placeholder='پایان' value={secondNumber} onChange={(e)=>setSecondNumber(e.target.value)} />
+        <input required type='number' placeholder='تعداد برنده ها' value={countWinner} onChange={(e)=>setCountWinner(e.target.value)} />
+        <input required type='number' placeholder='زمان' value={timeWinner} onChange={(e)=> setTimeWinner(e.target.value)} />
+        <button type='submit'>شروع قرعه کشی</button>
+        <button onClick={(e)=>resetForm(e)}>پاک کردن</button>
+        </form>
+        
+        </div>
+        
+        
+    </div>
+    <Modal
+        isOpen={seconderPopup}
+        onRequestClose={()=>setSeconderPopup(false)}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div>
+        <CountUp
+  start={0}
+  end={tempWinner}
+  duration={2.75} />
+      </div>
+      
+      </Modal>
       <Modal
         isOpen={initialPopup}
         onRequestClose={()=>setInitialPopup(false)}
@@ -67,9 +135,9 @@ function Lottery() {
         contentLabel="Example Modal"
       >
         <div style={{direction:"rtl"}}>
-            <h3> برنده شماره {countWon} </h3>
-            <h3> یک عدد صندلی تاشو شیدکو</h3>
-            <h4>شماره : {popupValue}</h4>
+            <h2 style={{color:"#084C7C"}}> برنده شماره {countWon} </h2>
+            
+            <h1>شماره : {popupValue}</h1>
       </div>
       
       </Modal>
@@ -88,26 +156,15 @@ function Lottery() {
       width={width}
       height={height}
     />}
-        <form onSubmit={(e)=>startLottery(e)} className={styles.lotteryFrom}>
-        <label>شماره اولین کارت قرعه کشی</label>
-        <input type='number' placeholder='شروع' value={firstNumber} onChange={(e)=>setFirstNumber(e.target.value)} />
-        <label>شماره آخرین کارت قرعه کشی</label>
-        <input type='number' placeholder='پایان' value={secondNumber} onChange={(e)=>setSecondNumber(e.target.value)} />
-        <button type='submit'>شروع قرعه کشی</button>
-        <button onClick={(e)=>resetForm(e)}>پاک کردن</button>
-        </form>
-        {showGif && <div className={styles.overlay}>
-        <img src={logomotion} alt='Loading' className={styles.gifImage} />
-      </div>}
-    <div>
-        <h3>نتیجه</h3>
-        <ul>
-        {randomNumbers.length > 0 && randomNumbers.map((number, index) => (
-          <li key={index}>{number}</li>
-        ))}
-      </ul>
+    {playMusic && <ReactAudioPlayer
+  src={effect}
+  autoPlay
+ loop={true}
+/>}
+    <div style={{display:"flex",justifyContent:"center",marginTop:"5vw", backgroundColor:"#F0F0F0", padding:"2px"}}>طراحی و توسعه در واحد دیجیتال مارکتینگ شیدکو</div>
     </div>
-    </div>
+   
+    </>
   )
 }
 
